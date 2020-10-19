f7ce67bdb45da514b327bc38d5d8704eead5afb3
MayAmatista <mosqueira.mayra@gmail.com>
2020-10-19 01:36:13 -0300

    clase1

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+
+function init() {
+  console.log("Loading server");
+
+  http
+    .createServer(function(request, response) {
+      console.log("Request received");
+      response.writeHead(200, { "Content-Type": "text/html" });
+      response.write("Hola Mundo asd");
+      response.end();
+    })
+    .listen(8888);
+}
+
+exports.init = init;
+
+
